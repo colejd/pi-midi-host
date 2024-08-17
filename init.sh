ebf70583950f2ac713dcbd6cf21f97246a87dda3
Jonathan Cole <702432+colejd@users.noreply.github.com>
2024-08-16 22:03:57 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 sudo apt-get install git -y
-git clone https://github.com/sinedied/pi-midi-host.git
+git clone https://github.com/colejd/pi-midi-host.git
 cd pi-midi-host
 
 echo "This will take a while, grab a cup of coffee..."
